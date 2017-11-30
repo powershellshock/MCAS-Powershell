34faa069f2a3ff70f48630f368d35486cef3dd42
Jared <jpoeppel@microsoft.com>
2017-11-30 18:21:51 -0500

    Starting test development

--- test/MCAS-Powershell.Tests.ps1
+++ test/MCAS-Powershell.Tests.ps1
@@ -1,4 +1,4 @@
-$ModuleManifestName = 'MCAS-Powershell.psd1'
+$ModuleManifestName = 'MCAS.psd1'
 $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 
 Describe 'Module Manifest Tests' {
@@ -8,3 +8,31 @@ Describe 'Module Manifest Tests' {
     }
 }
 
+$cmdsUsingCredParam = Get-Command -Module MCAS | ForEach-Object { if (($_.parameters.GetEnumerator() | Where-Object {$_.Key -eq 'Credential'}).Count -eq 1) {$_.Name}}
+
+
+<#
+Describe 'Validating -Credential parameter' {
+
+        Mock -ModuleName MCAS Get-Date { return (New-Object datetime(2000,1,1)) }
+
+        Context $cmd {
+            It "Should not accept a credential with an invalid MCAS tenant as the user name" {
+                {Get-MCASUserGroup -Credential (Import-Clixml "badtenanturi.credential")} | Should Throw 'Cannot validate argument on parameter'
+            }     
+
+            It "Should not accept a credential with a 63 char hex token as the password" {
+                {&($cmdExpression) -Credential (Import-Clixml "63charhex.credential")} | Should Throw 'Cannot validate argument on parameter'
+            }  
+
+            It "Should not accept a credential with a 65 char hex token as the password" {
+                {&($cmdExpression) -Credential (Import-Clixml "65charhex.credential")} | Should Throw 'Cannot validate argument on parameter'
+            } 
+            
+            It "Should not accept a credential with an 64 char non-hex as the password" {
+                {&($cmdExpression) -Credential (Import-Clixml "64charnonhex.credential")} | Should Throw 'Cannot validate argument on parameter'
+            }
+        }
+    }
+}
+#>
