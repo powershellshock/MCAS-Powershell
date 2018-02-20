bc354102487a085114338ac95ca25005aeff6509
Jared <jpoeppel@microsoft.com>
2018-02-20 15:34:50 -0500

    code style updates

--- functions/Get-MCASAppPermission.ps1
+++ functions/Get-MCASAppPermission.ps1
@@ -1,7 +1,7 @@
 function Get-MCASAppPermission
 {
     [CmdletBinding()]
-    Param
+    param
     (
         # Specifies the credential object containing tenant as username (e.g. 'contoso.us.portal.cloudappsecurity.com') and the 64-character hexadecimal Oauth token as the password.
         [Parameter(Mandatory=$false)]
@@ -19,7 +19,7 @@ function Get-MCASAppPermission
         [int]$Skip = 0
     )
     
-    $body = @{'skip'=$Skip;'limit'=$ResultSetSize} # Base request body
+    $body = @{'skip'=$Skip;'limit'=$ResultSetSize} # Request body
 
     try {
         $response = Invoke-MCASRestMethod -Credential $Credential -Path '/cas/api/v1/app_permissions/' -Method Post -Body $body
