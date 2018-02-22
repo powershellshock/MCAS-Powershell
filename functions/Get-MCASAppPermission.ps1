1d7b6a01016ae000541f96afa84c0fe7a55d0da3
Jared <jpoeppel@microsoft.com>
2018-02-21 20:58:29 -0500

    ready for v3

--- functions/Get-MCASAppPermission.ps1
+++ functions/Get-MCASAppPermission.ps1
@@ -16,20 +16,19 @@ function Get-MCASAppPermission
         # Specifies the number of records, from the beginning of the result set, to skip.
         [Parameter(Mandatory=$false)]
         [ValidateScript({$_ -gt -1})]
-        [int]$Skip = 0,
-
-        [Parameter(Mandatory=$false)]
-        [switch]$RetrieveAllRecords
+        [int]$Skip = 0
     )
 
     $body = @{'skip'=$Skip;'limit'=$ResultSetSize} # Request body
 
     try {
-        $response = Invoke-MCASRestMethod -Credential $Credential -Path '/cas/api/v1/app_permissions/' -Method Post -Body $body -Raw
+        $response = Invoke-MCASRestMethod -Credential $Credential -Path '/cas/api/v1/app_permissions/' -Method Post -Body $body
     }
     catch {
         throw "Error calling MCAS API. The exception was: $_"
     }
 
-    $response.data
+    $response = $response.data
+
+    $response
 }
