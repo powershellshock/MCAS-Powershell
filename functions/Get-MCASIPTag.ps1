7546c4a6b6f519481563d90b86ace2af4ca33e3c
Jared <jpoeppel@microsoft.com>
2018-02-28 12:01:04 -0500

    code style and comment cleanup

--- functions/Get-MCASIPTag.ps1
+++ functions/Get-MCASIPTag.ps1
@@ -8,8 +8,6 @@ function Get-MCASIPTag
         [ValidateNotNullOrEmpty()]
         [System.Management.Automation.PSCredential]$Credential = $CASCredential,
 
-
-
         # Specifies the maximum number of results to retrieve when listing items matching the specified filter criteria.
         [Parameter(Mandatory=$false)]
         [ValidateRange(1,100)]
@@ -24,24 +22,20 @@ function Get-MCASIPTag
 
         # Get the matching alerts and handle errors
         try {
-            $response = Invoke-MCASRestMethod -Credential $Credential -Path "/cas/api/tags/?enabledOnly=true&sort=name&sortDirectory=asc&target=ip" -Method Get # IP tag
-            #$response = Invoke-MCASRestMethod -Credential $Credential -Path "/cas/api/tags/?enabledOnly=true&sort=name&sortDirectory=asc&target=user" -Method Get # group
-            #$response = Invoke-MCASRestMethod -Credential $Credential -Path "/cas/api/tags/?enabledOnly=true&sort=name&sortDirectory=asc&target=device" -Method Get # device tag
-            #$response = Invoke-MCASRestMethod -Credential $Credential -Path "/cas/api/tags/?enabledOnly=true&sort=name&sortDirectory=asc&target=userAgent" -Method Get # useragent tag
-            
+            $response = Invoke-MCASRestMethod -Credential $Credential -Path "/cas/api/tags/?enabledOnly=true&sort=name&sortDirectory=asc&target=ip" -Method Get # IP tag          
         }
-            catch {
-                throw "Error calling MCAS API. The exception was: $_"
-            }
+        catch {
+            throw "Error calling MCAS API. The exception was: $_"
+        }
 
         Write-Verbose "Getting just the response property named 'data'"
         $response = $response.data
 
-        #Write-Verbose "Adding the friendly name of the application to the response as a property named 'app'"
-        #$Response = $Response | Add-Member -NotePropertyName 'app' -NotePropertyValue ($AppId -as [mcas_app]) -PassThru
-
-        #Write-Verbose "Selecting properties to be returned"
-        #$Response = $Response | Select-Object -Property name,app,types,id
+        try {
+            Write-Verbose "Adding alias property to results, if appropriate"
+            $response = $response | Add-Member -MemberType AliasProperty -Name Identity -Value '_id' -PassThru
+        }
+        catch {}
 
         $response
     }
