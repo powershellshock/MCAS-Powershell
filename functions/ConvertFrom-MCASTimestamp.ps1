e1a4d99e2d4cadf150e1d8ec536edb61539a820e
Jared <jpoeppel@microsoft.com>
2018-03-02 14:47:29 -0500

    code style and cleanup

--- functions/ConvertFrom-MCASTimestamp.ps1
+++ functions/ConvertFrom-MCASTimestamp.ps1
@@ -1,10 +1,7 @@
 function ConvertFrom-MCASTimestamp {
     [CmdletBinding()]
     [OutputType([datetime])]
-    Param (
-        [Parameter(Mandatory=$true, Position=0)]
-        $Timestamp
-        )
+    param ([Parameter(Mandatory=$true, Position=0)]$Timestamp)
 
-        (([datetime]'1/1/1970').AddSeconds($Timestamp/1000)).ToLocalTime()
+    (([datetime]'1/1/1970').AddSeconds($Timestamp/1000)).ToLocalTime()
 }
