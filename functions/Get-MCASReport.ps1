36fd499fb554b5dd5184acaa7e21fdfc2ab7011f
Jared <jpoeppel@microsoft.com>
2018-03-21 16:33:12 -0400

    Get-MCASReport functional

--- functions/Get-MCASReport.ps1
+++ functions/Get-MCASReport.ps1
@@ -10,7 +10,7 @@ function Get-MCASReport {
 
     # Get the matching items and handle errors
     try {
-        $response = Invoke-MCASRestMethod -Credential $Credential -Path "/api/reports/" -Method Post
+        $response = Invoke-MCASRestMethod -Credential $Credential -Path "/cas/api/reports/" -Method Get
     }
     catch {
         throw "Error calling MCAS API. The exception was: $_"
@@ -18,5 +18,11 @@ function Get-MCASReport {
 
     $response = $response.data
 
-    $response    
+    try {
+        Write-Verbose "Adding alias property to results, if appropriate"
+        $response = $response | Add-Member -MemberType AliasProperty -Name Identity -Value '_id' -PassThru
+    }
+    catch {}
+    
+    $response
 }
