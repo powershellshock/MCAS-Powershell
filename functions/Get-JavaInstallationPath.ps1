e1eb93e0bd884360ecec4f777275fc7513fc411e
Jared <jpoeppel@microsoft.com>
2018-06-07 14:18:43 -0400

    functionalized Java detection

---
+++ functions/Get-JavaInstallationPath.ps1
@@ -0,0 +1,20 @@
+function Get-JavaInstallationPath
+{   
+    [CmdletBinding()]
+
+    try {
+        Write-Verbose 'Checking installed programs list for an existing Java installation on this host.'    
+        $javaProductGuid = (Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match '^Java (?:8|9) Update \d{1,3}.*$'} | Sort-Object -Property Name -Descending | select -First 1).IdentifyingNumber
+        
+        if ($javaProductGuid) {
+            Write-Verbose "Java is installed. Getting the installation location from HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$javaProductGuid"         
+            (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$javaProductGuid" -Name 'InstallLocation').InstallLocation
+        }
+        else {
+            Write-Verbose "Java was not found in the installed programs list"
+        }
+    }
+    catch {
+        Write-Warning 'Something went wrong attempting to detect the Java installation or its installation path. The error was $_'
+    }
+}
