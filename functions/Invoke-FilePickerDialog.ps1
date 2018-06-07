e7080e6dc715ea11d4e72ec474bc9a7c70cfcd45
Jared <jpoeppel@microsoft.com>
2018-06-07 11:47:53 -0400

    finalized Invoke-FilePickerDialog

--- functions/Invoke-FilePickerDialog.ps1
+++ functions/Invoke-FilePickerDialog.ps1
@@ -1,20 +1,26 @@
 function Invoke-FilePickerDialog
 {   
+    [CmdletBinding()]
     param
     (
         # Specifies the directory in which the picker will begin
-        [string]$InitialDirectory = "C:\Program Files",
+        [string]$InitialDirectory = $PWD,
 
-        # Specifies the directory in which the picker will begin
-        [string]$Filter = 'All files|*.*'    
+        # Specifies the file filter to be used
+        [string]$Filter = 'All files|*.*',
+
+        # Specifies the title for the dialog window
+        [string]$Title
         )
-    $InitialDirectory = 
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
     $filePicker = New-Object System.Windows.Forms.OpenFileDialog
     
+    Write-Verbose "InitialDirectory for file picker is $InitialDirectory"
+    Write-Verbose "Filter for file picker is $Filter"
     $filePicker.initialDirectory = $InitialDirectory
     $filePicker.filter = $Filter
+    $filePicker.title = $Title
     $filePicker.ShowDialog() | Out-Null
     
     $filePicker.filename
