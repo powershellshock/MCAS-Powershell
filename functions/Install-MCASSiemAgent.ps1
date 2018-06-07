1dbc74bbe3608203ef52f36ab5e85c01f68a7274
Jared <jpoeppel@microsoft.com>
2018-06-07 11:48:00 -0400

    in progress

--- functions/Install-MCASSiemAgent.ps1
+++ functions/Install-MCASSiemAgent.ps1
@@ -72,46 +72,125 @@ function Install-MCASSiemAgent {
 
     Write-Verbose 'Attempting to detect an existing Java installation on this host.'
     # Check for Java
-    if (Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match '^Java (?:8|9) Update \d{1,3}.*$'}) {
+    $javaProduct = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match '^Java (?:8|9) Update \d{1,3}.*$'}
+    if ($javaProduct) {
 
-        # Java appears to be installed
+        Write-Verbose 'Java appears to be installed, based on the Add/Remove Programs list'
+        
+        
+        (Get-ItemProperty -Path ('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{0}' -f $javaProduct.IdentifyingNumber) -Name 'InstallLocation').InstallLocation
+
+
+
+
+
+
+        # 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{26A24AE4-039D-4CA4-87B4-2F64180171F0}\InstallLocation = C:\Program Files\Java\jre1.8.0_171\'
+        # (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{26A24AE4-039D-4CA4-87B4-2F64180171F0}' -Name 'InstallLocation').InstallLocation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Write-Verbose 'Attempting to detect an existing Java installation on this host.'
+
+        Write-Verbose 'Checking for Java in its default location (C:\Program Files\Java\jre*\bin\java.exe).'
+        $progFilesJava = "$env:ProgramFiles\Java"       
+        $javaExeFiles = (Get-ChildItem -Path $env:ProgramFiles -Recurse 'java.exe' -ErrorAction SilentlyContinue).FullName
+        $javaExePathMatchPattern = '^C:\\Program Files\\Java\\jre(?:\d|\.|_)+\\bin\\java.exe$'
+
+        if ($javaExeFiles.Count -eq 1 -and $javaExeFiles -match $javaExePathMatchPattern) {
+            Write-Verbose "One Java installation detected in Program Files"
+
+        }
+        elseif ($javaExeFiles.Count -gt 1) {
+            Write-Verbose "More than one Java installation detected in Program Files, so prompting the user to select one"
+            Invoke-FilePickerDialog -InitialDirectory 'C:\Program Files\Java' -Filter 'Java Runtime Engine|java.exe' -Title 'Select Java engine (java.exe) to be used by the MCAS SIEM Agent'
+        }
+        else {
+            Write-Verbose "No Java installation detected in Program Files, so prompting the user for the path to java.exe"
+            Invoke-FilePickerDialog -InitialDirectory 'C:\' -Filter 'Java Runtime Engine|java.exe' -Title 'Select Java engine (java.exe) to be used by the MCAS SIEM Agent'
+        }
+
 
         # check if java is in the PATH
         try {     
-            $JavaExePath = (Get-Item ($JavaExePath = '{0}\java.exe' -f (($env:PATH).split(';') | Where-Object {$_ -match '^.*Java\\.*\\bin$'} | Select -First 1))).FullName
+            #$JavaExePath = (Get-Item ($JavaExePath = '{0}\java.exe' -f (($env:PATH).split(';') | Where-Object {$_ -match '^.*Java\\.*\\bin$'} | Select -First 1))).FullName
         }
         catch {
 
         }
         
     }
-    
-    #$javaPath = 'C:\Program Files\Java\jre1.8.0_171\bin\java.exe'
-    
-    Invoke-FilePickerDialog -InitialDirectory "C:\Program Files\Java\" -Filter 'Java Runtime Engine|java.exe'
+    else {
+        Write-Verbose 'Java does NOT appear to be installed, based on the Add/Remove Programs list'
 
+        # Ask if Java has already been downloaded
+        $downloadJava = $false
 
-    try {
+        if ($downloadJava) {
+            Write-Verbose 'Attempting to download the Java installation package.'
+            try {
+                $javaDownloadUrl = ((Invoke-WebRequest -Uri 'https://www.java.com/en/download/manual.jsp' -UseBasicParsing).links | Where-Object {$_.title -eq 'Download Java software for Windows (64-bit)'} | select -Last 1).href
+                
+                if (Test-Path "$pwd\JavaSetup.tmp") {
+                    Remove-Item "$pwd\JavaSetup.tmp" -Force
+                }
+                
+                $javaDownloadResult = Invoke-WebRequest -Uri $javaDownloadUrl -UseBasicParsing -OutFile "$pwd\JavaSetup.tmp"
+                
+                $javaSetupFileName = (Get-Item "$pwd\JavaSetup.tmp").VersionInfo.OriginalFilename
+                
+                if (Test-Path "$pwd\$javaSetupFileName") {
+                    Remove-Item "$pwd\$javaSetupFileName" -Force
+                }
+                
+                Rename-Item "$pwd\$javaSetupFileName" -Force
+            }
+            catch {
+                throw "Something went wrong downloading the Java installation package. The error was $_"
+            }
+        }
+        else {
+            # Prompt for path to Java setup package
+            Invoke-FilePickerDialog -InitialDirectory "$env:userprofile\Downloads" -Filter 'EXE files|*.exe' -Title 'Select Java setup package to be installed for the MCAS SIEM Agent'
+        }
 
-    }
-    catch {
-
-    }
-
-
-
-
-
-    Write-Verbose 'Attempting to download the Java installation package.'
-    try {
-        $javaDownloadUrl = ((Invoke-WebRequest -Uri 'https://www.java.com/en/download/manual.jsp' -UseBasicParsing).links | Where-Object {$_.title -eq 'Download Java software for Windows (64-bit)'} | select -Last 1).href
-        $javaDownloadResult = Invoke-WebRequest -Uri $javaDownloadUrl -UseBasicParsing -OutFile "$pwd\JavaSetup.tmp"
-        $javaSetupFileName = (Get-Item "$pwd\JavaSetup.tmp").VersionInfo.OriginalFilename
-        Rename-Item "$pwd\JavaSetup.tmp" $javaSetupFileName -Force
-    }
-    catch {
         
+        # Ask if Java setup should be interactive or silent
+
+        # Install Java
+
+
     }
+    
+
+
+    #$javaPath = 'C:\Program Files\Java\jre1.8.0_171\bin\java.exe'
+
+    Get-ChildItem
+    
+    Invoke-FilePickerDialog -InitialDirectory 'C:\Program Files\Java' -Filter 'Java Runtime Engine|java.exe' -Verbose
+
+
+
+
+
+
+
+
 
 
     Write-Verbose 'Attempting to install Java on this host.'
